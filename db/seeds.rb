db9bcfc42cabd0ab8a3fcd97d9fbacbf5ca3dd93
Lucivaldo Castro <lucivaldocb@gmail.com>
2024-02-09 19:34:21 -0300

    Configura os campos created_at e updated_at ao criar dados fakers

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,24 +11,37 @@
 puts "Geração de autores"
 
 10.times do
+  created_at = Faker::Date.between(from: 1.month.ago, to: DateTime.current)
+  updated_at = Faker::Date.between(from: created_at, to: DateTime.current)
+
   Author.create(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
-    title: Faker::Name.prefix
+    title: Faker::Name.prefix,
+    created_at:,
+    updated_at:
   )
 end
 
 puts "Geração de fornecedores"
 
 10.times do
+  created_at = Faker::Date.between(from: 1.month.ago, to: DateTime.current)
+  updated_at = Faker::Date.between(from: created_at, to: DateTime.current)
+
   Supplier.create(
-    name: Faker::Company.name
+    name: Faker::Company.name,
+    created_at:,
+    updated_at:
   )
 end
 
 puts "Geração de clientes"
 
 10.times do
+  created_at = Faker::Date.between(from: 1.month.ago, to: DateTime.current)
+  updated_at = Faker::Date.between(from: created_at, to: DateTime.current)
+
   Customer.create(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
@@ -36,7 +49,9 @@ puts "Geração de clientes"
     email: Faker::Internet.email,
     visits: Faker::Number.between(from: 1, to: 100),
     orders_count: Faker::Number.between(from: 1, to: 20),
-    lock_version: Faker::Number.between(from: 0, to: 10)
+    lock_version: Faker::Number.between(from: 0, to: 10),
+    created_at:,
+    updated_at:
   )
 end
 
@@ -45,6 +60,8 @@ puts "Geração de livros"
 10.times do
   author = Author.order('RANDOM()').first
   supplier = Supplier.order('RANDOM()').first
+  created_at = Faker::Date.between(from: 1.month.ago, to: DateTime.current)
+  updated_at = Faker::Date.between(from: created_at, to: DateTime.current)
 
   Book.create(
     title: Faker::Book.title,
@@ -54,7 +71,9 @@ puts "Geração de livros"
     out_of_print: Faker::Boolean.boolean(true_ratio: 0.2),
     views: Faker::Number.between(from: 1, to: 1000),
     author_id: author.id,
-    supplier_id: supplier.id
+    supplier_id: supplier.id,
+    created_at:,
+    updated_at:
   )
 end
 
@@ -62,6 +81,8 @@ puts "Geração de pedidos"
 
 10.times do
   customer = Customer.order('RANDOM()').first
+  created_at = Faker::Date.between(from: 1.month.ago, to: DateTime.current)
+  updated_at = Faker::Date.between(from: created_at, to: DateTime.current)
 
   Order.create(
     date_submitted: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
@@ -70,7 +91,9 @@ puts "Geração de pedidos"
     shipping: Faker::Commerce.price(range: 1..100, as_string: false),
     tax: Faker::Commerce.price(range: 1..100, as_string: false),
     total: Faker::Commerce.price(range: 100..2000, as_string: false),
-    customer_id: customer.id
+    customer_id: customer.id,
+    created_at:,
+    updated_at:
   )
 end
 
@@ -79,6 +102,8 @@ puts "Geração de avaliações"
 10.times do
   customer = Customer.order('RANDOM()').first
   book = Book.order('RANDOM()').first
+  created_at = Faker::Date.between(from: 1.month.ago, to: DateTime.current)
+  updated_at = Faker::Date.between(from: created_at, to: DateTime.current)
 
   Review.create(
     title: Faker::Lorem.sentence,
@@ -86,6 +111,8 @@ puts "Geração de avaliações"
     rating: Faker::Number.between(from: 1, to: 5),
     state: Faker::Number.between(from: 0, to: 2),
     customer_id: customer.id,
-    book_id: book.id
+    book_id: book.id,
+    created_at:,
+    updated_at:
   )
 end
